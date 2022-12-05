cbf177e6b5f62050f33c40e2d4c52ab81407c4de
liyinan2333 <liyinan2333@qq.com>
2022-12-05 18:47:46 +0800

    ActiveMQ script.

--- install-activemq.sh
+++ install-activemq.sh
@@ -1,3 +1,13 @@
-mkdir -p ~/app/activemq/data/
-mkdir -p ~/app/activemq/log/
+mkdir -p ~/app/amq1/conf/
+mkdir -p ~/app/amq1/data/
+mkdir -p ~/app/amq1/log/
+mkdir -p ~/app/amq2/conf/
+mkdir -p ~/app/amq2/data/
+mkdir -p ~/app/amq2/log/
+mkdir -p ~/app/amq3/conf/
+mkdir -p ~/app/amq3/data/
+mkdir -p ~/app/amq3/log/
+cp ./activemq/activemq.xml ~/app/amq1/conf/activemq.xml
+cp ./activemq/activemq.xml ~/app/amq2/conf/activemq.xml
+cp ./activemq/activemq.xml ~/app/amq3/conf/activemq.xml
 docker-compose -f activemq/docker-compose-activemq.yml up -d --remove-orphans
