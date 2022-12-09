ad68fef540638a2bf5448867d6bbf051acc338d1
liyinan2333 <liyinan2333@qq.com>
2022-12-09 18:45:53 +0800

    ActiveMQ cluster script.

--- install-activemq.sh
+++ install-activemq.sh
@@ -1,13 +1,6 @@
-mkdir -p ~/app/amq1/conf/
-mkdir -p ~/app/amq1/data/
-mkdir -p ~/app/amq1/log/
-mkdir -p ~/app/amq2/conf/
-mkdir -p ~/app/amq2/data/
-mkdir -p ~/app/amq2/log/
-mkdir -p ~/app/amq3/conf/
-mkdir -p ~/app/amq3/data/
-mkdir -p ~/app/amq3/log/
-cp ./activemq/activemq.xml ~/app/amq1/conf/activemq.xml
-cp ./activemq/activemq.xml ~/app/amq2/conf/activemq.xml
-cp ./activemq/activemq.xml ~/app/amq3/conf/activemq.xml
+count=3
+mkdir -pv `seq -f $HOME'/app/amq%1g/conf/data' 1 $count`
+mkdir -pv `seq -f $HOME'/app/amq%1g/conf/log' 1 $count`
+mkdir -pv `seq -f $HOME'/app/amq%1g/conf/conf' 1 $count`
+find ~/app -wholename '*amq[1-3]/conf' | xargs -n 1 cp -v ./activemq/activemq.xml
 docker-compose -f zookeeper/docker-compose-zookeeper.yml -f activemq/docker-compose-activemq.yml up -d --remove-orphans
