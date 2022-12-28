f82b41b730ece573b580c62c60f350c4c72a7289
liyinan2333 <liyinan2333@qq.com>
2022-12-28 10:36:47 +0800

    Update activemq installer scripts.

--- install-activemq.sh
+++ install-activemq.sh
@@ -1,6 +1,6 @@
 count=3
-mkdir -pv `seq -f $HOME'/app/amq%1g/conf/data' 1 $count`
-mkdir -pv `seq -f $HOME'/app/amq%1g/conf/log' 1 $count`
-mkdir -pv `seq -f $HOME'/app/amq%1g/conf/conf' 1 $count`
+mkdir -pv `seq -f $HOME'/app/amq%1g/data' 1 $count`
+mkdir -pv `seq -f $HOME'/app/amq%1g/log' 1 $count`
+mkdir -pv `seq -f $HOME'/app/amq%1g/conf' 1 $count`
 find ~/app -wholename '*amq[1-3]/conf' | xargs -n 1 cp -v ./activemq/activemq.xml
 docker-compose -f zookeeper/docker-compose-zookeeper.yml -f activemq/docker-compose-activemq.yml up -d --remove-orphans
