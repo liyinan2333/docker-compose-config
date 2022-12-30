e6fedcb2bde5d01e5330e69973d7d1c7dd7ea913
liyinan2333 <liyinan2333@qq.com>
2022-12-30 10:07:37 +0800

    Update redis installer script.

--- install-redis-sentinel.sh
+++ install-redis-sentinel.sh
@@ -1,15 +1,17 @@
 count=3
-node_names=`seq -f 'redis_node_%1g' 1 $count`
-#创建目录
-mkdir -pv `seq -f $HOME'/app/redis_node_%1g/conf' 1 $count`
-mkdir -pv `seq -f $HOME'/app/redis_node_%1g/data' 1 $count`
-mkdir -pv `seq -f $HOME'/app/redis_sentinel_%1g/data' 1 $count`
-mkdir -pv `seq -f $HOME'/app/redis_sentinel_%1g/conf' 1 $count`
-#复制配置文件
-redis_node_conf_path=`seq -f $HOME'/app/redis_node_%1g/conf/redis.conf' 1 $count`
-redis_sentinel_conf_path=`seq -f $HOME'/app/redis_sentinel_%1g/conf/sentinel.conf' 1 $count`
-echo $redis_node_conf_path | xargs -n 1 cp -v ./redis/redis.conf
-echo $redis_sentinel_conf_path | xargs -n 1 cp -v ./redis/sentinel.conf
-#修改配置文件
-echo $node_names | xargs -0 -n1 -i sed -i 's/replica-announce-ip 127.0.0.1/replica-announce-ip '{}'/g' $HOME'/app/'{}'/conf/redis.conf'
+node_name=redis_node_
+sentinel_name=redis_sentinel_
+i=0
+while [ $i -lt $count ]
+do
+  i=$(( $i + 1 ))
+  mkdir -pv $HOME/app/$node_name$i/conf
+  mkdir -pv $HOME/app/$node_name$i/data
+  mkdir -pv $HOME/app/$sentinel_name$i/data
+  mkdir -pv $HOME/app/$sentinel_name$i/conf
+  cp -v ./redis/redis.conf $HOME/app/$node_name$i/conf/redis.conf
+  cp -v ./redis/sentinel.conf $HOME/app/$sentinel_name$i/conf/sentinel.conf
+  sed -i 's/replica-announce-ip 127.0.0.1/replica-announce-ip '$node_name$i'/g' $HOME/app/$node_name$i/conf/redis.conf
+  sed -i 's/sentinel announce-ip 127.0.0.1/sentinel announce-ip '$sentinel_name$i'/g' $HOME/app/$sentinel_name$i/conf/sentinel.conf
+done
 docker-compose -f redis/docker-compose-redis-node.yml -f redis/docker-compose-redis-sentinel.yml up -d --remove-orphans
