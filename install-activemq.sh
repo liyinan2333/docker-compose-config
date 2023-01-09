e63dc2016ee2091de606d6858b68353148f46c38
liyinan2333 <liyinan2333@qq.com>
2023-01-09 18:09:28 +0800

    Update.

--- install-activemq.sh
+++ install-activemq.sh
@@ -1,6 +1,9 @@
 count=3
-mkdir -pv `seq -f $HOME'/app/amq%1g/data' 1 $count`
-mkdir -pv `seq -f $HOME'/app/amq%1g/log' 1 $count`
-mkdir -pv `seq -f $HOME'/app/amq%1g/conf' 1 $count`
-find ~/app -wholename '*amq[1-3]/conf' | xargs -n 1 cp -v ./activemq/activemq.xml
+network_name=activemq
+mkdir -pv `seq -f "$HOME/app/amq%1g/data" 1 $count`
+mkdir -pv `seq -f "$HOME/app/amq%1g/log" 1 $count`
+mkdir -pv `seq -f "$HOME/app/amq%1g/conf" 1 $count`
+find ~/app -wholename "*amq[1-3]/conf" | xargs -n 1 cp -v ./activemq/activemq.xml
+./init_network.sh zookeeper
+./init_network.sh activemq
 docker-compose -f zookeeper/docker-compose-zookeeper.yml -f activemq/docker-compose-activemq.yml up -d --remove-orphans
--- install-zookeeper.sh
+++ install-zookeeper.sh
@@ -1,7 +1,7 @@
 count=3
 network_name=zookeeper
-mkdir -pv `seq -f $HOME'/app/zk%1g/conf' 1 $count`
-mkdir -pv `seq -f $HOME'/app/zk%1g/data' 1 $count`
-find ~/app -wholename '*zk[1-3]/conf' | xargs -n 1 cp -v ./zookeeper/zoo.cfg
+mkdir -pv `seq -f "$HOME/app/zk%1g/conf" 1 $count`
+mkdir -pv `seq -f "$HOME/app/zk%1g/data" 1 $count`
+find ~/app -wholename "*zk[1-3]/conf" | xargs -n 1 cp -v ./zookeeper/zoo.cfg
 ./init_network.sh $network_name
 docker-compose -f zookeeper/docker-compose-zookeeper.yml -f zookeeper/docker-compose-zookeeper.yml up -d --remove-orphans
